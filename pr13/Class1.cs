541f53806297c8aaaef99a9ee490c41c7628c6ba
ИСП-31 <-31@LAB17-08>
2023-11-20 12:34:45 +0300

    Добавьте файлы проекта.

---
+++ pr13/Class1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr13
+{
+    public class Soluiton
+    {
+        public static int GetResult(int[,] matrix)
+        {
+            int count1, count2, result = 0;
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                count1 = 0; count2 = 0;
+                
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    if (matrix[i, j] < 0) count1++;
+                    else count2++;
+                }
+
+                if (count1 == count2) result = j + 1;
+            }
+
+            return result;
+        }
+    }
+
+    public class ArrayMod
+    {
+        public static void FillRandon(ref int[,] matrix)
+        {
+            Random rnd = new();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(-10, 10);
+                }
+            }
+        }
+
+        public static void Save(int[,] mas)
+        {
+            SaveFileDialog save = new();
+            save.DefaultExt = ".txt";
+            save.Filter = "Все файлы (*.*) | *.* | Текстовые файлы | *.txt";
+            save.FilterIndex = 2;
+            save.Title = "Сохранение таблицы";
+
+            if (save.ShowDialog() == true)
+            {
+                StreamWriter file = new(save.FileName);
+                file.WriteLine(mas.GetLength(0));
+                file.WriteLine(mas.GetLength(1));
+
+                foreach (int i in mas)
+                {
+                    file.WriteLine(i);
+                }
+                file.Close();
+            }
+        }
+
+        public static int[,]? Open()
+        {
+            OpenFileDialog open = new();
+            open.DefaultExt = ".txt";
+            open.Filter = "Все файлы (*.*) | *.* | Текстовые файлы | *.txt";
+            open.FilterIndex = 2;
+            open.Title = "Открытие таблицы";
+
+            if (open.ShowDialog() == true)
+            {
+                StreamReader file = new(open.FileName);
+
+                int length1 = Convert.ToInt32(file.ReadLine());
+                int length2 = Convert.ToInt32(file.ReadLine());
+
+                int[,] mas = new int[length1, length2];
+
+                for (int i = 0; i < length1; i++)
+                {
+                    for (int j = 0; j < length2; j++)
+                    {
+                        mas[i, j] = Convert.ToInt32(file.ReadLine());
+                    }
+                }
+                file.Close();
+                return mas;
+            }
+            return null;
+
+        }
+    }
+}
---
+++ pr13/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace pr13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        DispatcherTimer timer;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+
+            tbNumber.Text = "Практическая работа №13";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            tbTime.Text = date.ToString("HH:mm");
+            tbDate.Text = date.ToString("dd.MM.yyyy");
+        }
+
+        private void btnFIll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
